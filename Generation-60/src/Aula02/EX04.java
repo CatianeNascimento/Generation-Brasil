e78c1936b9c9e8b0c1d27c6bb186b1e2a0716d5b
CatianeNascimento <csnascimento07@gmail.com>
2023-02-08 16:54:45 -0300

    aula 2

---
+++ Generation-60/src/Aula02/EX04.java
@@ -0,0 +1,32 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class EX04 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+		
+		double a, b, c, d, r, s, somaR, somaS; 
+		
+		System.out.println("Insira 3 valores: ");
+		System.out.println("VALOR A: ");
+		a = input.nextDouble();
+		System.out.println("VALOR B: ");
+		b = input.nextDouble();
+		System.out.println("VALOR C: ");
+		c = input.nextDouble();
+		
+		somaR = a + b;
+		somaS = b + c;
+		
+		r = Math.pow(somaR, 2);
+		s = Math.pow(somaS, 2);
+		
+		d = (r + s) / 2;
+		
+		System.out.println("O resultado do cálculo é: " + d);
+		
+	}
+
+}
---
+++ Generation-60/src/Aula02/EX07.java
@@ -0,0 +1,37 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class EX07 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in); 
+		
+		double a, b, c, d, e, f, x, y; 
+		
+		System.out.println("Digite o valor de a: ");
+		a = input.nextDouble();
+		
+		System.out.println("Digite o valor de b: ");
+		b = input.nextDouble();
+		
+		System.out.println("Digite o valor de c: ");
+		c = input.nextDouble();
+		
+		System.out.println("Digite o valor de d: ");
+		d = input.nextDouble();
+		
+		System.out.println("Digite o valor de e: ");
+		e = input.nextDouble();
+		
+		System.out.println("Digite o valor de f: ");
+		f = input.nextDouble();
+		
+		x = (c * e) - (b * f) / (a * e) - (b * d); 
+		y = (a * f) - (c * d) / (a * e) - (b * d);
+		
+		System.out.printf("O valor de x é: %.2f e de y é: %.2f ", x, y);
+	}
+
+}
